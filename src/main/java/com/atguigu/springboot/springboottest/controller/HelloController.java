d47c2d6b120622719abe7fe54a2413d799be5874
root <root@139.com>
2019-11-18 01:58:23 +0800

    idea初始化提交

---
+++ src/main/java/com/atguigu/springboot/springboottest/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springboot.springboottest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@RestController
+public class HelloController {
+    @Autowired
+    DataSource dataSource;
+
+    @RequestMapping("/hello")
+    public  String test(String user){
+        System.out.println("user = [" + user + "]");
+        HashMap<Object, Object> map = new HashMap<>();
+        
+        System.out.println("dataSource = " + dataSource);
+        return  "hello";
+    }
+}
---
+++ src/test/java/com/atguigu/springboot/springboottest/SpringboottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
